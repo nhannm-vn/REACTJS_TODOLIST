81e6b83694fba97db844562281c0f821ad2c736a
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2024-12-12 07:02:27 +0700

    update 12-12-2024

--- src/components/TaskInput/TaskInput.tsx
+++ src/components/TaskInput/TaskInput.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react'
 import { TaskInputProps } from '../../@types/props.types'
 import styles from './taskInput.module.scss'
 function TaskInput(props: TaskInputProps) {
+  //_tạo state name để có chỗ mà lưu trữ giá trị của
+  //value ô input cũng như đảm bảo dữ liệu được hiện ra chính xác
+  //_Lưu ý khi đã có value thì cần thêm hàm onChange(bắt buộc) để khi dữ
+  //liệu thay đổi thì nó render ra cùng lúc
+  const [name, setName] = useState<string>('')
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //_Lấy value hiện tại của ô input
+    const { value } = event.target
+    //_setState lại
+    setName(value)
+  }
+
   return (
     <div className='mb-2'>
       <h1 className={`${styles.title} mb-2`}>To do list typescript</h1>
       <form className={styles.form}>
-        <input type='text' placeholder='caption goes here' />
+        <input
+          type='text' //
+          placeholder='caption goes here'
+          value={name}
+          onChange={onChangeInput}
+        />
         <button type='submit'>➕</button>
       </form>
     </div>
